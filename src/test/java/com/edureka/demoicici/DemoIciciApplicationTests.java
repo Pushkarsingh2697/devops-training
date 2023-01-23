089c11428c86cfe1aea03e974a1f98595b21a53e
catchrameshhere <58819447+catchrameshhere@users.noreply.github.com>
2023-01-23 14:58:15 +0530

    initial commit

---
+++ src/test/java/com/edureka/demoicici/DemoIciciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edureka.demoicici;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoIciciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
